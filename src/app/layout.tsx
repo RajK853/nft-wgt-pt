09904c4126abcfc450ee87be93ac2aa181fc7d5e
Raj Kumar Rana <rgk.rana@gmail.com>
2025-10-26 23:27:45 +0100

    refactor: Remove empty navigation bar from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Removed empty navigation bar */}
         {children}
       </body>
     </html>
